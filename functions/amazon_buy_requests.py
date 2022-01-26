efed058c5a555d4a748003c18405c235a747ba45
Dom <56895762+G4mmasG4me@users.noreply.github.com>
2022-01-26 10:57:31 +0000

    small work, some comments on form payload

--- functions/amazon_buy_requests.py
+++ functions/amazon_buy_requests.py
@@ -1,13 +1,22 @@
 import requests
 
 def buy_product(item_order, order_queue, product_link, amazon_signed_in_session):
-  pass
-  
+  # load product url into amazon session
+  amazon_signed_in_session.get(product_link)
 
-  # load website in requests session
-  # check if 
+  # do inital check to see if it is available
+
   # unfilled_order_item_id = item_order[0]
   # return [unfilled_order_item_id, status]
+
+  # get all other parameters
+  # then insert these into the payload
+
+  # quantity: 1
+  # submit.buy-now: Submit
+
+
+
 
 def buy_basket():
   pass
