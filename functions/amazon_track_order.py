4ce6fbdbe1cceb7d2ba6b808481e9edf6f6fa73e
G4mmasG4me <domhough@hotmail.co.uk>
2022-02-01 07:40:18 +0000

    tracking order notes

---
+++ functions/amazon_track_order.py
@@ -0,0 +1,3 @@
+# https://www.amazon.co.uk/gp/css/summary/print.html?orderID=206-9680814-4401159
+# https://www.amazon.co.uk/documents/download/68de87e0-de59-445a-96bb-bde851bf0572/invoice.pdf
+# amazon page to get order summary and invoice
