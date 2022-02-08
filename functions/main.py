91275154e63a4f85a6f8e9773ec28f11c08b9e7b
GammasGame <domhough@hotmail.co.uk>
2022-02-08 01:06:28 +0000

    minor tweaks to main file

--- functions/main.py
+++ functions/main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     
     start = time.time()
     # select a list of product_ids and their country where they were ordered from
-    mycursor.execute('SELECT DISTINCT order_item.product_id, product_link.id, product_link.link, address.country  FROM unfilled_order_item INNER JOIN order_item ON unfilled_order_item.order_item_id = order_item.id INNER JOIN address ON order_item.address_id = address.id INNER JOIN product_link ON (product_link.product_id = order_item.product_id AND product_link.region = address.country)')
+    mycursor.execute('SELECT DISTINCT order_item.product_id, product_link.id, product_link.link, address.country FROM unfilled_order_item INNER JOIN order_item ON unfilled_order_item.order_item_id = order_item.id INNER JOIN address ON order_item.address_id = address.id INNER JOIN product_link ON (product_link.product_id = order_item.product_id AND product_link.region = address.country)')
 
     # fetches all results from mysql server
     unique_order_items = mycursor.fetchall()
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     # :
     order_items_queue = multiprocessing.Queue()
     for order_item_position, unique_order in enumerate(unique_order_items):
-      p = multiprocessing.Process(target=amazon_check_stock_requests.check_stock_amazon, args=(unique_order, order_items_queue, order_item_position,)) # stock_checker.check_stock
+      p = multiprocessing.Process(target=amazon_check_stock_requests.check_stock_amazon, args=(unique_order[2], order_items_queue, order_item_position,)) # stock_checker.check_stock
       chekck_availability_processes.append(p)
       p.start()
     for p in chekck_availability_processes:
