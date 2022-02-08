6567ba0c3ecf16f3f957cac94ddce4151e6ded2a
Dom <56895762+G4mmasG4me@users.noreply.github.com>
2022-02-08 15:53:06 +0000

    main file upgrade

--- functions/main.py
+++ functions/main.py
@@ -4,6 +4,7 @@ import multiprocessing
 import time
 import requests
 
+# custom functions imports
 import amazon_check_stock_requests
 import amazon_sign_in_selenium
 import amazon_buy_requests
@@ -24,9 +25,7 @@ running = True
 
 
 if __name__ == '__main__':
-  print('test')
-  manager = multiprocessing.Manager()
-  while running:
+  while running is True:
     
     start = time.time()
     # select a list of product_ids and their country where they were ordered from
@@ -34,25 +33,22 @@ if __name__ == '__main__':
 
     # fetches all results from mysql server
     unique_order_items = mycursor.fetchall()
-
-    # creates an output list filled with 0s of the amount of unique orders
-    # output = manager.list([0]*len(unique_orders))
     
     # seperates the different components of unique orders
     product_ids, link_ids, product_links, order_regions = map(list, zip(*unique_order_items))
 
-    chekck_availability_processes = []
+    check_availability_processes = []
     # :
     order_items_queue = multiprocessing.Queue()
     for order_item_position, unique_order in enumerate(unique_order_items):
       p = multiprocessing.Process(target=amazon_check_stock_requests.check_stock_amazon, args=(unique_order[2], order_items_queue, order_item_position,)) # stock_checker.check_stock
-      chekck_availability_processes.append(p)
+      check_availability_processes.append(p)
       p.start()
-    for p in chekck_availability_processes:
+    for p in check_availability_processes:
       p.join()
 
     availability_output = []
-    for p in chekck_availability_processes:
+    for p in check_availability_processes:
       availability_output.append(order_items_queue.get())
     print(availability_output)
 
@@ -66,14 +62,12 @@ if __name__ == '__main__':
       amazon_signed_in, amazon_sign_in_cookies = amazon_sign_in_selenium.sign_in_amazon()
 
 
-    if amazon_signed_in:
+    if amazon_signed_in is True:
       # start requests session
       amazon_session = requests.Session()
       # laod cookies into session
       for cookie in amazon_sign_in_cookies:
         amazon_session.amazon_sign_in_cookies.set(cookie['name'], cookie['value'])
-
-      item_available = False
 
       # create order queue
       order_queue = multiprocessing.Queue()
@@ -81,8 +75,7 @@ if __name__ == '__main__':
       buy_item_processes = []
       for availability_position, availability in enumerate(availability_output):
         # if the product is available to order
-        if availability[1] == True:
-          item_available = True
+        if availability is True:
           order_item = unique_order_items[availability_position]
           product_link = order_item[2]
           product_region = order_item[3]
@@ -91,29 +84,27 @@ if __name__ == '__main__':
           mycursor.execute(sql, (order_item[0], order_item[3])) # parameters are product id and link region
           item_orders = mycursor.fetchall()
 
-          
-          
           for item_order_position, item_order in enumerate(item_orders):
             # item order = [unfilled_order_item_id, order_item_id, address_line_1, address_line_2, address_postcode, address_city, address_county]
             p = multiprocessing.Process(target=amazon_buy_requests.buy_product, args=(item_order, order_queue, product_link, amazon_session))
             buy_item_processes.append(p)
       
-      if item_available:
+      if buy_item_processes: # if any of the items in the list are available
         
-        for p in buy_item_processes:
+        for p in buy_item_processes: # start buy processes
           p.start()
-        for p in buy_item_processes:
+        for p in buy_item_processes: # join buy processes
           p.join()
           
         order_output = []
-        for p in buy_item_processes:
+        for p in buy_item_processes: # get output for buy processes
           order_output.append(order_items_queue.get())
 
         email_processes = []
         email_queue = multiprocessing.Queue()
         # each item in order_output should look like [unfilled_order_item_id, order_placed, status, details]
         for unfilled_order_item_id, order_item_id, order_placed, status, details in order_output:
-          if order_placed: # if order successfully placed
+          if order_placed is True: # if order successfully placed
             # move unfilled_order_item to filled_order_item
             sql = ('BEGIN TRANSACTION; INSERT INTO filled_order_item (order_item_id) SELECT (order_item_id) FROM unfilled_order_item WHERE unfilled_order_item.id = %s; DELETE FROM unfilled_order_item WHERE unfilled_order_item.id = %s; COMMIT;')
             mycursor.execute(sql, (unfilled_order_item_id, unfilled_order_item_id)) # parameters are product id and link region
@@ -137,3 +128,10 @@ if __name__ == '__main__':
         email_output = []
         for p in email_processes:
           email_output.append(email_queue.get())
+
+
+# process
+# user buys service
+# check availability on chosen product
+# if available then buy product
+# if bought then change details in db, and email user
