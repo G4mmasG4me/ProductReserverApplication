e22b1b55f5c80a1de686145829cbe2231f03ddbc
GammasGame <domhough@hotmail.co.uk>
2022-02-15 07:42:33 +0000

    notes

---
+++ functions/bot_generator.py
@@ -0,0 +1,7 @@
+# used to generate bot instructions
+
+# run script
+# then it starts a browser for you
+# on that browser it records any steps you make
+# then it turns those steps into actions and saves them
+# you can then select those actions that you want to be on the bot
