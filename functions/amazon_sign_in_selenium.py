25d4296d243aae90a306a25706030db0dd717e46
Dom <56895762+G4mmasG4me@users.noreply.github.com>
2022-03-09 12:38:03 +0000

    more sign in additions

--- functions/amazon_sign_in_selenium.py
+++ functions/amazon_sign_in_selenium.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -5,111 +6,75 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from urllib.parse import urlparse
 import os
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-extensions')
-# chrome_options.add_argument('--start-maximized)
-# chrome_options.add_argument('--incognito)
-# chrome_options.add_argument('--disable-popup-blocking)
-# chrome_options.add_argument('--disable-infobars)
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
 
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s, options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.getcwd() + r"\chromedriver.exe", options=chrome_options)
 
-amazon_sign_in_url = 'https://www.amazon.com/gp/sign-in.html'
-amazon_sign_in_check_url = '/ap/signin'
+amazon_sign_in_url_goto = '/gp/sign-in.html'
+amazon_sign_in_url = '/ap/signin'
+amazon_account_fix_up_url = '/ap/accountfixup'
 
 sign_in_email = 'productreservertest@gmail.com'
 sign_in_password = 'ProdReserve2022'
 
 def sign_in_amazon():
-  try:
-    driver.get(amazon_sign_in_url)
-    if urlparse(driver.current_url).path == amazon_sign_in_check_url: # if currently on sign in page
-      try: # try signing in
-        email_input = driver.find_element(By.ID, 'ap_email')
-        email_input.send_keys(sign_in_email)
-        email_input.submit()
-        if urlparse(driver.current_url).path == amazon_sign_in_check_url: # if currently on sign in page
-          try: # tries to find if there is an error message, i.e. wrong email
-            driver.find_element(By.ID, 'auth-error-message-box')
-            return [False, 'Wrong Email']
-          except NoSuchElementException as e: # if no error message box
-            try: # tries to input password and submit
-              password_input = driver.find_element(By.ID, 'ap_password')
-              password_input.send_keys(sign_in_password)
-              password_input.submit()
-              try: # tries to find if there is an error message, i.e. wrong passwrod
-                driver.find_element(By.ID, 'auth-warning-message-box')
-                return [False, 'Wrong Password']
-              except NoSuchElementException as e: # no error box |
+  driver.get(amazon_sign_in_url)
+  if urlparse(driver.current_url).path == amazon_sign_in_url: # if currently on sign in page
+    try: # try signing in
+      email_input = driver.find_element_by_id('ap_email')
+      email_input.send_keys(sign_in_email)
+      email_input.submit()
+      if urlparse(driver.current_url).path == amazon_sign_in_url: # if currently on sign in page
+        try: # tries to find if there is an error message, i.e. wrong email
+          driver.find_element_by_id('auth-error-message-box')
+          return [False, 'Wrong Email']
+        except NoSuchElementException as e: # if no error message box
+          try: # tries to input password and submit
+            password_input = driver.find_element_by_id('ap_password')
+            password_input.send_keys(sign_in_password)
+            password_input.submit()
+            if urlparse(driver.current_url).path == '':
+              pass
+            
+            # --- Phone Add Form --- #
+            elif urlparse(driver.current_url).path == amazon_account_fix_up_url:
+              try:
+                add_phone_form = driver.find_element(By.ID, 'auth-account-fixup-phone-form')
                 try:
-                  # find captcha box
-                  captcha_box = driver.find_element(By.ID, 'captcha-form-xyz-change-me') # change captcha form id once known
-                  return [False, 'Captcha'] # cant currently solve captcha, develop ML to solve amazon captcha
-                  # upon solving captcha
-                  if urlparse(driver.current_url).netloc == 'www.amazon.com':
-                    ubid_cookie = driver.get_cookie('ubid-main')
-                    x_cookie = driver.get_cookie('x-main')
-                    session_token_cookie = driver.get_cookie('session-token')
-                    return [True, [ubid_cookie, x_cookie, session_token_cookie]]
-                  else:
-                    return [False, 'Error Signing In']
-                except NoSuchElementException: # successfull login
-                  if urlparse(driver.current_url).netloc == 'www.amazon.com':
-                    ubid_cookie = driver.get_cookie('ubid-main')
-                    x_cookie = driver.get_cookie('x-main')
-                    session_token_cookie = driver.get_cookie('session-token')
-                    return [True, [ubid_cookie, x_cookie, session_token_cookie]]
-                  else:
-                    return [False, 'Error Signing In']
-                except Exception as e:
-                  return [False, 'Error Solving Captcha: %s' % e]
-            except NoSuchElementException as e: # if no password input box
-              return [False, 'No Password Input'] 
-            except Exception as e:
-              return [False, 'Unable to Input Password: %s' % e]
-        else: # not on sign in page
-          return [False, 'Not On Sign In Page']
-      except NoSuchElementException as e: # if email input box not found
-        return [False, 'No Email Input']
-      except Exception as e:
-        return [False, 'Unable to Input Email: %s' % e]
-    else:
-      return [False, 'Not on Sign In Page']
-  finally:
-    driver.close()
+                  skip_phone_button = driver.find_element(By.ID, 'ap-account-fixup-phone-skip-link')
+                except NoSuchElementException as e:
+                  return [False, 'Error Finding Skip Phone Button']
+              except NoSuchElementException as e:
+                return [False, 'Error Finding Add Phone Box']
 
-if __name__ == '__main__':
-  status, cookies_or_error = sign_in_amazon()
-  if status is True:
-    print('Status: %s' % status)
-    print('Cookies: %s' % cookies_or_error)
-  elif status is False:
-    print('Status: %s' % status)
-    print('Error: %s' % cookies_or_error)
-  else:
-    print('Unknown Error')
-    print('Unknown Status: %s' % status)
-    print('Cookies or Error: %s' % cookies_or_error)
-  quit()
-# cookies required to keep login | .co.uk
+            # --- Still On Login Page --- #
+            elif urlparse(driver.current_url).path == : # tries to find if there is an error message, i.e. wrong passwrod
+              driver.find_element_by_id('auth-warning-message-box')
+              return [False, 'Wrong Password']
+            except NoSuchElementException as e: # no error box
+              cookies = driver.get_cookies()
+              return [True, cookies]
+              # check if logged in
+              # may have mobile number popup
+          except NoSuchElementException as e: # if no password input box
+            return [False, 'No Password Input'] 
+      else: # not on sign in page
+        return [False, 'Not On Sign In Page']
+    except NoSuchElementException as e: # if email input box not found
+      return [False, 'No Email Input']
+
+
+
+output = sign_in_amazon()
+print(output)
+
+# cookies required to keep login
 # ubid-acbuk
 # x-acbuk
 # session-token
 # if you delete these cookies, then you get logged out
-
-# cookies required to keep login | .com
-# ubid-main
-# x-mainW
-# session-token
-
-# cookies required to keep login | .fr
-# ubid-abcfr
-# x-acbfr
-# session-token
