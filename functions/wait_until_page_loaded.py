e2d056c4e6b244ecf0fbebb2932551fe87fff398
GammasGame <domhough@hotmail.co.uk>
2022-03-15 00:55:13 +0000

    page load function

---
+++ functions/wait_until_page_loaded.py
@@ -0,0 +1,10 @@
+import time
+
+def wait_until_page_loaded(driver, max_attempts):
+  attempts = 0
+  page_state = driver.execute_script('return document.readyState;')
+  while page_state != 'complete' or attempts >= max_attempts:
+    page_state = driver.execute_script('return document.readyState;')
+    attempts += 1
+    print('Attempt')
+    time.sleep(0.1)
