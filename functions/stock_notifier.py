76ba7e51ed080b62fda4a9439f955f04b336a3db
GammasGame <domhough@hotmail.co.uk>
2022-03-22 08:04:09 +0000

    eddited sql, added emailing process

--- functions/stock_notifier.py
+++ functions/stock_notifier.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     
     start = time.time()
     # select a list of product_ids and their country where they were ordered from
-    mycursor.execute('SELECT DISTINCT order_item.product_id, product_link.id, product_link.link, address.country FROM unfilled_order_item INNER JOIN order_item ON unfilled_order_item.order_item_id = order_item.id INNER JOIN address ON order_item.address_id = address.id INNER JOIN product_link WHERE (product_link.product_id = order_item.product_id AND product_link.region = address.country)')
+    mycursor.execute('SELECT DISTINCT order_item.product_id, product_link.id, product_link.link, product_link.last_availability, address.country FROM unfilled_order_item INNER JOIN order_item ON unfilled_order_item.order_item_id = order_item.id INNER JOIN address ON order_item.address_id = address.id INNER JOIN product_link WHERE (product_link.product_id = order_item.product_id AND product_link.region = address.country)')
 
     # fetches all results from mysql server
     unique_order_items = mycursor.fetchall()
@@ -66,13 +66,23 @@ if __name__ == '__main__':
         product_region = order_item[3]
 
         # get a list of all emails based on their product and region
+        # get a list of all the data for that particular product
         sql = 'SELECT DISTINCT user.email FROM availability_mailing_list INNER JOIN user ON availability_mailing_list.user_id = user.id INNER JOIN address ON user.id = address.user_id WHERE (availability_mailing_list.product_id = %s AND user.country = %s)'
         mycursor.execute(sql, (product_id, product_region)) # parameters are product id and link region
         emails = mycursor.fetchall()
 
+        sql = 'SELECT * FROM products WHERE id = %s'
+        mycursor.execute(sql, (product_id))
+        product_details = mycursor.fetchall()
+        product_details = tuple(zip(*zip(*product_details), product_link)) # adds extra column to 2d product list
+
         for email in emails:
-          pass
-            
+          p = multiprocessing.Process(target=email_customer.send_stock_alert, args=(email, product_details))
+          email_processes.append(p)
+          p.start()
+      
+    for p in email_processes:
+      p.join()
 
 
 # process
@@ -80,3 +90,11 @@ if __name__ == '__main__':
 # check availability on chosen product
 # if available then buy product
 # if bought then change details in db, and email user
+
+
+
+
+# check if stock has changed since last tmie
+# if has changed to true then send email
+# if has changed to false then don't do anything
+# if hasn't changed then don't do anything
